197c8174a53e3aba6c3fb0a50dabfa56ce449d54
Abdul Qayyum <kaien@alyssaa.net>
2016-01-12 15:51:11 +0800

    updated to move SSMAT to it's on github project

--- SSMAT/app.js
+++
@@ -1,7 +0,0 @@
-var _this = this;
-window.onload = function () {
-    global_style = { font: "14px Courier Bold", fill: "#FFFFFF", wordWrap: false, wordWrapWidth: _this.width, align: "left" };
-    global_game = new SSMAT.Game();
-    admin_no = document.getElementById("admin_no").value;
-    restartFromLeaderboard = false;
-};

a53118bf65dfa4f2b090770156adc39781f2bf75
kaienx <kaienx@MSI>
2016-07-17 02:20:57 +0800

    test

---
+++ lennylah/js/grayscale.js
@@ -0,0 +1,49 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
+
+$(document).ready(function () {
+  
+    $('#fullpage').fullpage({
+            //Navigation
+            //anchors:['intro', 'about'],
+            navigation: false,
+            navigationPosition: 'left',
+            navigationTooltips: ['intro', 'about'],
+            showActiveTooltip: false,
+            slidesNavigation: false,
+            slidesNavPosition: 'bottom',
+            onLeave: function (index, nextIndex, direction) {
+                var leavingSection = $(this);
+
+                //after leaving section 2
+                if (index == 2 && direction == 'down') {
+                }
+                else if (index == 2 && direction == 'up') {
+                }
+            }
+    });
+    $('.overlay').delay(1000).fadeOut(1200);
+});
+
+//$('#myModal').modal('show');
